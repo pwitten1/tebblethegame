a07a13568f195993b3ced9ee68f3e855693453f9
Paul Witten <paulwitten@nat-oitwireless-inside-vapornet100-b-10533.Princeton.EDU>
2017-04-16 12:49:27 -0400

    Grid improvements

--- Tebble/Grid.js
+++ Tebble/Grid.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import {View, StyleSheet, Text} from 'react-native';
+import Cell from './cell';
 
 export class Grid extends Component{
+
+        constructor(props) {
+        super(props);
+        this.state = {
+            w: props.w,
+            h: props.h,
+            grid: [],
+            score: 0,
+            started: false,
+            gameOver: true
+        }
+        this.grid = [];
+        this.speed = 450;
+
+    }
+
+    createGrid() {
+        const {w, h} = this.state;
+        var grid = [];
+        var row = [];
+
+        for(i = 1; i <= h; i++) { //h is 20, so i want 20 rows
+            for(j = 1; j <= w; j++) { // w is 10
+                var cell = 0;
+                row.push(cell);
+            }
+            grid.push(row);
+            row = [];
+        }
+        this.grid = grid;
+        this.setState({grid}, () => {
+        });
+    }
+
+
+
 	render(){
 		return (
             <View style={styles.container}>
