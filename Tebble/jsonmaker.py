b38b6e6ea78b32151ba15a2113bd5cea023af40c
pwitten1 <pwitten@princeton.edu>
2017-05-14 23:33:42 -0400

    Delete jsonmaker.py

--- Tebble/jsonmaker.py
+++
@@ -1,9 +0,0 @@
-import json
-file = open("dictionary.txt")
-arr = []
-txt = file.readline().upper()
-while txt != "":
-	arr.append(txt[:len(txt)-1])
-	txt = file.readline().upper()
-son = json.dumps(arr)
-print son
